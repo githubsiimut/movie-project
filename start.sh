e8af28106f0602dd0d0dde8fbb149fb7f1c52c12
siimut <mfathin.insan279@gmail.com>
2024-06-29 12:30:34 +0700

    First Commit

--- start.sh
+++ start.sh
@@ -2,17 +2,18 @@ set -eu
 
 export PYTHONUNBUFFERED=true
 
-VIRTUALENV=.data/venv
+VIRTUALENV.data/venv
 
-if [! -d $VIRTUALENV ]; then
-    python3 -m venv $VIRTUALENV
+if [ -d SVIRTUALENV ]; then python3 -m veny SVIRTUALENV
+
 fi
 
-if [ ! -f $VIRTUALENV/bin/pip ]; then
-    curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/bin/python
+if [ -f $VIRTUALENV/bin/pip]; then curl --silent --show-error--retry 5 https://bootstrap.pypa.io/get-pip.py | SVIRTUALE
+
 fi
 
-$VIRTUALENV/bin/pip install -r requirements.txt
+$SVIRTUALENV/bin/pip install -r requirements.txt
 
-$VIRTUALENV/bin/python3 app.py
-footer
+$SVIRTUALENV/bin/python3 app.py
+
+Footer
